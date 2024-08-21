514193b87ff0a19519dde92ea95265c599b493d5
Amaan <amaanzaidi462@gmail.com>
2024-08-21 15:05:33 +0530

    SetUp-done

---
+++ healthcare-system/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+
+const font = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "CareSync",
+  description: "A HealthCare Management App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen font-sans bg-dark-300 antialiased",
+          font.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
